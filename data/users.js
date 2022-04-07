f4f76c84086bd303eb12e5eab69dd1901a660ef0
suleman-suraj <sulemansuraj1233@gmail.com>
2022-04-07 21:59:34 +0000

    first commit

---
+++ data/users.js
@@ -0,0 +1,8 @@
+const users=[
+    {id:1, name:"Suraj", age:25},
+    {id:2, name:"Oscar", age:27},
+    {id:3, name:"Nazifa", age:22},
+    {id:4, name:"Aida", age:20},
+    {id:5, name:"Felix", age:19}
+]
+module.exports=users
---
+++ routes/usersRoutes.js
@@ -0,0 +1,14 @@
+const {Router} =require("express")
+
+const {getUsers, createUser, getUser} =require("../controllers/usersController")
+
+const router =Router();
+
+router.route("/users").get(getUsers).post(createUser)
+router.get("/users/:id", getUser)
+
+
+
+
+
+module.exports=router
